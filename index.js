77281a62ba2ad152ccca968d84ca27865981803b
Mike Whit <2973130+mikegwhit@users.noreply.github.com>
2018-09-24 14:02:43 -0600

    Adds the core package.json, and also fixes a bug in the scripts bin/ncli script.

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    cache: require('ncli-core-cache'),
+    configs: require('ncli-core-configs'),
+    console: require('ncli-core-console'),
+    files: require('ncli-core-files'),
+    logger: require('ncli-core-logger'),
+    rscandir: require('ncli-core-rscandir'),
+    scripts: require('ncli-core-scripts')
+};
---
+++ scripts/safeinstall.js
@@ -0,0 +1,38 @@
+/**
+ * @fileoverview This file combats weird NPM and Yarn errors relating to local
+ * filesystem lib.  What it does is delete the lock files, redownload the 
+ * dependencies IN ORDER such that the lowest dependency is rebuilt first.  It 
+ * seemed like once a low-level dependency had been corrupted other installs 
+ * would fail.  This script solves for that.
+ */
+const cp = require('child_process');
+const fs = require('fs');
+const chalk = require('chalk');
+
+const safeInstall = (dir) => {
+    const cwd = process.cwd();
+    process.chdir(dir);
+    console.log(chalk.cyan('Safely Installing'), dir);
+    try {fs.unlinkSync('./package-lock.json');} catch(e) {}
+    try {
+        cp.execSync('rm -rf node_modules', {encoding: 'utf8', stdio: 'ignore'});
+    } catch(e) {
+        console.log(chalk.red('Failure Occurred'), dir);
+    }
+    try {
+        cp.execSync('npm i', {encoding: 'utf8', stdio: 'ignore'});
+    } catch(e) {
+        console.log(chalk.red('Failure Occurred'), dir);
+    }
+    process.chdir(cwd);
+}
+safeInstall(__dirname + '/../lib/rscandir');
+safeInstall(__dirname + '/../lib/files');
+safeInstall(__dirname + '/../lib/helpers');
+safeInstall(__dirname + '/../lib/configs');
+safeInstall(__dirname + '/../lib/cache');
+safeInstall(__dirname + '/../lib/logger');
+safeInstall(__dirname + '/../lib/console');
+safeInstall(__dirname + '/../lib/scripts');
+safeInstall(__dirname + '/../');
+require('ncli-core-helpers').Shutdown.start();
