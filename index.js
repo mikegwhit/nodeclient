4ed95878cfaa937e9d0eeb996159409443d50f59
Mike Whit <2973130+mikegwhit@users.noreply.github.com>
2018-10-15 13:05:53 -0600

    Adds the createFileSegments/joinFileParts functions within files module.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/**
+ * @fileoverview It is important to check if the .cache file exists in the
+ * main directory.
+ */
+
 module.exports = {
     cache: require(__dirname + '/lib/core/cache'),
     configs: require(__dirname + '/lib/core/configs'),
--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,29 +1,111 @@
 /**
- * Builds the cache for both the package index AND the files index for all 
- * packages.
- * @fileoverview
+ * @fileoverview This file combats weird NPM and Yarn errors relating to local
+ * filesystem lib.  What it does is delete the lock files, redownload the 
+ * dependencies IN ORDER such that the lowest dependency is rebuilt first.  It 
+ * seemed like once a low-level dependency had been corrupted other installs 
+ * would fail.  This script solves for that.
  */
-
-const Files = require('ncli-core-files');
-const Cache = require('ncli-core-cache');
+const cp = require('child_process');
+const fs = require('fs');
 const chalk = require('chalk');
-const Shutdown = require('ncli-core-helpers').Shutdown;
-console.log(chalk.yellow('Caching Configs'));
-Files.readAllFiles('configs');
-console.log(chalk.yellow('Caching Scripts'));
-Files.readAllFiles('scripts');
-console.log(chalk.yellow('Caching Timers'));
-Files.readAllFiles('timers');
-console.log(chalk.yellow('Caching Keys'));
-Files.readAllFiles('keys');
-Cache.options('localFiles', {
-    persist: true
+const ProgressBar = require('progress');
+
+let progress = null;
+const initProgress = (total, name) => {
+    progress = new ProgressBar(':bar ' +
+        chalk.grey('(:current/:total packages) ') + chalk.yellow(':label'), {
+            label: name,
+            total,
+            width: 40
+        });
+};
+
+const installPackage = (dir) => {
+    const cwd = process.cwd();
+    process.chdir(dir);
+    return new Promise((resolve, reject) => {
+        const pkgJSON = 
+            JSON.parse(require('fs').readFileSync(dir + '/package.json', 'utf8'));
+        try {
+            cp.exec(`npm i ${dir} --save`).on('close', () => {
+                try {
+                    require('fs')
+                        .rmdirSync(`"${process.env['HOME']}/.node_modules/` +
+                        `${pkgJSON['name']}"`);
+                } catch(e) {}
+                if (process.platform != 'win32') {
+                    cp.exec(`ln -s "${require('path').resolve(dir)}" ` + 
+                        `"${process.env['HOME']}/.node_modules/${pkgJSON['name']}"`, 
+                        {encoding: 'utf8', stdio: 'ignore'}).on('close', () => {
+                            resolve();
+                        });
+                } else {
+                    const prefix = cp.execSync(`cmd /k "npm config get prefix"`);
+                    const home = cp.execSync(`cmd /k "echo %HOMEPATH%"`);
+                    cp.execSync(`cmd /k "mklink /D \"${home}/.node_modules/${pkgJSON['name']}\" \"${prefix}/node_modules/${pkgJSON['name']}\"`)
+                }
+                
+            });
+        } catch(e) {
+            resolve();
+        }
+    });
+}
+let packages = [
+    __dirname + '/../lib/core/rscandir',
+    __dirname + '/../lib/core/files',
+    __dirname + '/../lib/core/helpers',
+    __dirname + '/../lib/core/configs',
+    __dirname + '/../lib/core/cache',
+    __dirname + '/../lib/core/logger',
+    __dirname + '/../lib/core/console',
+    __dirname + '/../lib/core/scripts'
+];
+let promises = [];
+
+if (!require('fs').existsSync(`${process.env['HOME']}/.node_modules`)) {
+    require('fs').mkdirSync(`${process.env['HOME']}/.node_modules`);
+}
+
+console.log(chalk.cyan('Installing Nodeclient core'));
+packages.map((pkg) => {
+    if (!progress) {
+        initProgress(packages.length, '');
+        progress.tick({label: ''});
+    }
+    let packageName = pkg.split('/').pop();
+    try {
+        const pkgJSON = 
+            JSON.parse(require('fs').readFileSync(pkg + '/package.json', 'utf8'));
+        packageName = pkgJSON['name']; /* || pkgJSON['name']; */
+    } catch(e) {
+    }
+    let promise = installPackage(pkg);
+    promise.then(() => {
+        progress.tick({label: packageName});
+    });
+    promises.push(installPackage(pkg));
 });
-Cache.options('packages', {
-    persist: true
+Promise.all(promises).then(() => {
+    try {
+        // Remove the Nodeclient libraries first.
+        try {
+            require('fs')
+                .unlinkSync(`"${process.env['HOME']}/.node_modules/nodeclient`);
+        } catch(e) {
+            try {
+                cp.execSync(`rm -rf "${process.env['HOME']}/.node_modules/nodeclient"`);
+            } catch(e) {}
+        }
+        if (process.platform != 'win32') {
+            cp.execSync(`ln -s "${require('path').resolve(__dirname + '/../')}" ` + 
+                `"${process.env['HOME']}/.node_modules/nodeclient"`)
+        } else {
+            const prefix = cp.execSync(`cmd /k "npm config get prefix"`);
+            const home = cp.execSync(`cmd /k "echo %HOMEPATH%"`);
+            cp.execSync(`cmd /k "mklink /D \"${home}/.node_modules/nodeclient\" \"${prefix}/node_modules/nodeclient\"`)
+        }
+    } catch(e) {
+    }
+    process.exit();
 });
-Cache.options('packageObjects', {
-    persist: true
-});
-console.log(chalk.green('Done'));
-Shutdown.start();
